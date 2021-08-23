4ffc15c5db2c2ecb41d17ad559c62dcb54edd340
Ikhwan Yusoff <ikhwan.yusoff@aemenersol.com>
2021-08-23 17:10:49 +0800

    Test: Added new API; GetNewAPI

--- AEM Sandbox/Controllers/WeatherForecastController.cs
+++ AEM Sandbox/Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace AEM_Sandbox.Controllers
             })
             .ToArray();
         }
+        
+        [HttpGet("GetNewAPI")]
+        public IEnumerable<WeatherForecast> GetNewAPI()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
     }
 }
